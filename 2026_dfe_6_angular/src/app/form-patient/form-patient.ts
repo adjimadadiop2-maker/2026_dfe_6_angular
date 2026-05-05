68306493da9e93ebe6787ec9cd8d79eb02174f08
Adji Mada Diop <adjimadadiop2@gmail.com>
2026-05-05 20:47:03 +0000

    Ajout projet Angular final

---
+++ 2026_dfe_6_angular/src/app/form-patient/form-patient.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PatientService } from '../patient-service';
+import { ActivatedRoute as ActivateRouter } from '@angular/router';
+@Component({
+  selector: 'app-form-patient',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './form-patient.html',
+  styleUrl: './form-patient.css',
+})
+export class FormPatient {
+  isedit = false;
+  idpatient: number = 0;
+  private activatedRoute = inject(ActivateRouter);
+  formPatient=new FormGroup({
+    prenom : new FormControl(''),
+    nom : new FormControl(''),
+    mail : new FormControl(''),
+    age : new FormControl('')  
+  });
+
+   private http = inject(HttpClient);
+   private router = inject(Router);
+   private patientService = inject(PatientService);
+
+  
+
+  inputFormPatient(){
+    console.log("tester");
+    console.log(this.formPatient.value);
+    let data = this.formPatient.value;
+    this.patientService.storePatient(data).subscribe(()=>
+    console.log("patient enregistré avec succès"));
+    this.router.navigate(['patient']); 
+  }
+ 
+
+
+
+}
