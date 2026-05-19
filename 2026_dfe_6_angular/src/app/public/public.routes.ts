b2ee83a9b1b13b344fa65ceaa003f59987c45922
Adji Mada Diop <adjimadadiop2@gmail.com>
2026-05-19 09:39:38 +0000

    Initial commit Angular app

--- 2026_dfe_6_angular/src/app/public/public.routes.ts
+++ 2026_dfe_6_angular/src/app/public/public.routes.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { Acceuil } from '../acceuil/acceuil';                 
 import { Routes } from '@angular/router';
+import { Acceuil } from '../acceuil/acceuil';
 import { Publiclayout } from '../layouts/publiclayout/publiclayout';
+impo
 
 export const PublicRoutes: Routes = [
- {path :'',component:Publiclayout},
-  {path : 'acceuil', component: Acceuil},
-  {path :'registrer',component: Acceuil},
-  {path :'auth',component: Acceuil},
- 
- 
+  {
+    path: '',
+    component: Publiclayout,
+    children: [
+      {
+        path: '',
+        component: Acceuil
+      },
+      {
+        path: 'acceuil',
+        component: Acceuil
+      },
+      {
+        path: 'auth',
+        loadComponent: () =>
+          import('../auth/auth').then(m => m.Auth)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('../auth/register').then(m => m.Register)
+      }
+    ]
+  }
 ];
